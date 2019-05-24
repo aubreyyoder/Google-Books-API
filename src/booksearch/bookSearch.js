040cb030f76b87a7dd126604a9c6a23689c5e35a
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-05-23 19:10:41 -0700

    Fetching API

--- src/booksearch/bookSearch.js
+++ src/booksearch/bookSearch.js
@@ -1,23 +1,58 @@
 import React from "react";
 import "./bookSearch.css";
 import SearchBar from "../searchbar/searchBar";
-import FeatureBar from "../featurebar/featureBar";
 import BookList from "../booklist/bookList";
-import { tsConstructorType } from "@babel/types";
 
 class Booksearch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      author: "C. Warren Hollister",
-      price: "$50.00"
+      books: []
     };
+  }
+
+  componentDidMount() {
+    const url = "https://www.googleapis.com/books/v1/volumes";
+    const options = {
+      method: "GET",
+      headers: {
+        q: "",
+        filter: "",
+        printType: "",
+        key: "AIzaSyDFVzV17KZPMnzwTAt8ftgH_DUlWoKtIv8"
+      }
+    };
+    fetch(url, options)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Something went wrong! Please try again later!");
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          book: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+  searchBook(book) {
+    this.setState({
+      books: [...this.state.books, book]
+    });
   }
   render() {
     return (
       <div className="booksearch">
         <SearchBar />
-        <BookList price={this.state.price} author={this.state.author} />
+        <BookList handleSearch={book => this.searchBook(book)} />
       </div>
     );
   }
